fa6da2acc56e300f319102f84377f017dc607799
shashank <shekhar1245shashank@gmail.com>
2021-02-18 21:07:43 +0530

    Dom tree traversal

---
+++ classroom_stuff/dom/traversal/script.js
@@ -0,0 +1,33 @@
+
+
+/*
+
+let li=temp0
+undefined
+li.parentElement
+<ul>
+
+li.parentElement.parentElement
+<ul>
+
+li.parentElement.parentElement.parentElement
+<body data-new-gr-c-s-check-loaded="8.869.0" data-gr-ext-installed="">
+
+li.parentElement.siblling
+undefined
+li.parentElement.appendChild('p')
+Uncaught TypeError: Node.appendChild: Argument 1 is not an object.
+    <anonymous> debugger eval code:1
+debugger eval code:1:18
+li.parentElement.appendChild('li')
+Uncaught TypeError: Node.appendChild: Argument 1 is not an object.
+    <anonymous> debugger eval code:1
+debugger eval code:1:18
+li.parentElement.children
+HTMLCollection { 0: li, 1: li, 2: li
+, length: 3 }
+
+li.parentElement.children.appendChild
+undefined
+
+*/
